8a1ab3b322d352ca20f20766c0d0aa87e45e903e
Glydric <git@miglio.dev>
2023-08-23 13:25:51 +0200

    Initial Commit

---
+++ MacWorkspaces/MacWorkspacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacWorkspacesApp.swift
+//  MacWorkspaces
+//
+//  Created by Leonardo Migliorelli on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MacWorkspacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
